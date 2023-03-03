eca0a291e1d67239f2228b256b494fede88e2956
sunqihang <qihang130681@gmail.com>
2023-03-03 21:54:47 +0800

    Complete deque.h, and prepare stack and queue.

---
+++ MiniSTL/queue.h
@@ -0,0 +1,8 @@
+//
+// Created by QIHANG SUN on 2023/3/3.
+//
+
+#ifndef MINISTL_QUEUE_H
+#define MINISTL_QUEUE_H
+
+#endif //MINISTL_QUEUE_H
---
+++ MiniSTL/stack.h
@@ -0,0 +1,8 @@
+//
+// Created by QIHANG SUN on 2023/3/3.
+//
+
+#ifndef MINISTL_STACK_H
+#define MINISTL_STACK_H
+
+#endif //MINISTL_STACK_H
